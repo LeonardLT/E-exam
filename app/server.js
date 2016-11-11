532d2e4f670c54bdc9bc5527639c8005956c96f7
Leonard LiuTao <364125168@qq.com>
2016-11-11 19:47:05 +0800

    modify

---
+++ app/server.js
@@ -0,0 +1,42 @@
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const compiler = webpack(webpackConfig);
+
+app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    lazy: false,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: true
+    },
+    publicPath: webpackConfig.output.publicPath
+}));
+
+app.use(webpackHotMiddleware(compiler, {
+    log: console.log
+}));
+
+app.use(express.static('./public'));
+
+app.get('/hello', function (req, res) {
+    res.send('Hello, world!');
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, '..') + '/public/index.html')
+});
+
+app.get('/admin', function (req, res) {
+    res.sendFile(path.resolve(__dirname, '..') + '/public/admin.html')
+});
+
+// app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Listening on 3000');
+});
