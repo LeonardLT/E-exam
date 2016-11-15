adc2808b24f400ed78cf4351d61128bc9dd6da8e
Leonard LiuTao <364125168@qq.com>
2016-11-15 20:13:28 +0800

    modify

--- app/db/db.js
+++ app/db/db.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 module.exports = {
     connect: function (mode, callback) {
         let url = process.env.PROD_MONGODB || 'mongodb://localhost/exam-db';
-        // let url = process.env.PROD_MONGODB || 'mongodb://leonard:leonard@ds151117.mlab.com:51117/exam-db';
         if (mode === 'test') {
             url = 'mongodb://localhost/exam-test-db';
         }
